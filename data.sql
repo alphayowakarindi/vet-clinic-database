d4b197b0f6a36510ff6acf43bf225e9b398e82f5
Alphy Gacheru <alphygacheru@gmail.com>
2022-10-05 16:44:57 +0300

    Insert data into the species table

--- data.sql
+++ data.sql
@@ -28,6 +28,8 @@ INSERT INTO owners(full_name, age) VALUES ('Melody Pond', 77);
 INSERT INTO owners(full_name, age) VALUES ('Dean Winchester', 14);
 INSERT INTO owners(full_name, age) VALUES ('Jodie Whittaker', 38);
 
-
+-- Insert into the species table
+INSERT INTO species(name) VALUES ('Pokemon');
+INSERT INTO species(name) VALUES ('Digimon');
 
 
