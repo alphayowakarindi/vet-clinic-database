2a322ccb37210fc22b4c01dc9c0765c4bc301daa
Alphy Gacheru <alphygacheru@gmail.com>
2022-10-10 17:38:43 +0300

    Decrease execution time

--- schema.sql
+++ schema.sql
@@ -74,3 +74,8 @@ INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id F
 
 -- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
 insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+-- Decreasing query speed
+CREATE INDEX visits_of_animal_idx ON visits (animal_id); 
+CREATE INDEX email_index ON owners(email); 
+CREATE INDEX vet_id_index ON visits(vet_id ASC);
